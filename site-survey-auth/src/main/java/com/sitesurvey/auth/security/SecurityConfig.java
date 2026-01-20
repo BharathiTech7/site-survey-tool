7823fc91ebe900aa0e9e09a6ac73291fd45bec94
bharathi <reddybharathi599@gmail.com>
2026-01-20 21:16:07 +0530

    security commit

--- site-survey-auth/src/main/java/com/sitesurvey/auth/security/SecurityConfig.java
+++ site-survey-auth/src/main/java/com/sitesurvey/auth/security/SecurityConfig.java
@@ -67,7 +67,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "https://voluble-fudge-e77e26.netlify.app"
+        	));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
